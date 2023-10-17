339baad26d5924da30a743e6becd192d89aa00fe
Chaw Thiri San <113085742+chaw-thiri@users.noreply.github.com>
2023-10-17 20:45:53 +0900

    add comments

--- Lab07ex1.cpp
+++ Lab07ex1.cpp
@@ -3,11 +3,11 @@ using namespace std;
 class Rectangle {
 private:
 	double m_width = 0;
-	double m_height = 0;
+	double m_height = 0; // class member declaration
 public:
-	Rectangle* setWidth(double w) {
+	Rectangle* setWidth(double w) { 
 		m_width = w;
-		return this;
+		return this;   // returning object using this pointer so that other function can be called on this returned  (chaining)
 	}
 	Rectangle* setHeight(double h) {
 		m_height = h;
@@ -20,6 +20,6 @@ public:
 int main() {
 	Rectangle rect;
 	rect.Display();
-	rect.setWidth(10)->setHeight(20)->Display();
+	rect.setWidth(10)->setHeight(20)->Display();   // chaining using this pointer
 	return 0;
 }
