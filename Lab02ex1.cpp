023c73eb68e327c8b5973def158714d412c90a22
Chaw Thiri San <113085742+chaw-thiri@users.noreply.github.com>
2023-12-08 09:22:21 -0800

    add error handlng

--- Lab02ex1.cpp
+++ Lab02ex1.cpp
@@ -1,49 +1,43 @@
-
+// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
+void inputArr(int*,int);
+int searchArr(int*,int,int);
+int main() {
+    int size, ele, idx;
+    int* buf = NULL;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+    // allocating the memory buffer + error handling
+    try{
+        buf = new int [size];
+    }
+    catch (bad_alloc){
+        cout << "The size is too big.";
+        return 0;
+    }
+    // using the allocated memory 
+    cout << "Enter elements of the array :"; 
+    inputArr(buf, size);
+    cout << "Enter element to search : ";
+    cin >> ele;
+    idx = searchArr(buf, size, ele);
+    if (idx == -1 ) cout <<ele <<" does not exist in the array.";
+    else cout << ele << " is found in index " << idx <<".";
+    delete[] buf;
 
-//function prototypes
-void inputArray(int*, int);
-int searchIdx(int*, int, int);
-
-int main(){
-	int size, element, index;
-
-	int* buf = nullptr;
-
-	cout << "Enter the size of array : ";
-	cin >> size;
-
-	// allocate the memory buffer
-	buf = new int[size]; // giving a value to the pointer, add an array to the pointer
-	
-
-
-	// use the allocated memory 
-cout << "Enter elements of the array: ";
-	inputArray(buf, size);
-
-	cout << "Enter element to search : ";
-	cin >> element;
-	index = searchIdx(buf, size, element);
-
-	if (index == -1)
-		cout << element << " does not exist in the array.";
-	else
-		cout << element << " is found in index" << index << ".";
-
-	// free allocated meemry 
-	delete[] buf; // delete pointer array
+    return 0;
 }
-void inputArray(int* buf, int size) {
-	for (int i = 0; i < size; ++i)
-		cin >> *(buf + i);
-
+void inputArr(int* arr,int size){
+    for (int i = 0; i < size; ++i){
+        cin >> *(arr+i);  // arr[i]
+    }
+    
 }
-int searchIdx(int* buf, int size, int element) {
-	for (int i = 0; i < size; ++i) {
-		if (*(buf + i) == element)
-			return i;
-
-	}
-	return -1;
+int searchArr(int* arr,int size,int ele){
+    for (int i = 0; i < size; ++i){
+        if (arr[i] == ele ) 
+        return i;
+    }
+    return -1;
+}
