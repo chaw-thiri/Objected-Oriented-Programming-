ceb8c29e53373249781a7a715806dd440cee8ce8
Chaw Thiri San <113085742+chaw-thiri@users.noreply.github.com>
2023-12-09 02:30:57 +0900

    added error handling

--- Lab02ex2.cpp
+++ Lab02ex2.cpp
@@ -13,20 +13,25 @@ int main() {
 	cin >> row >> col;
 
 	// allocate the memory buffers for the three matrices
-	mat1 = new int[row*col];
-	mat2 = new int[row*col];
-	matAdd = new int[row*col];
-
-	// use the buffers
-	cout << "Enter elements in 1st matric of size" << row << 'x' << col << ":\n";
-	MatInput(mat1, row, col);
-	cout << "Enter elements in 2nd matric of size" << row << 'x' << col << ":\n";
-	MatInput(mat2, row, col);
-
-	MatAdd(mat1, mat2, matAdd, row, col); 
-
-	cout << "Sum of two matrices \n";
-	MatDisplay(matAdd, row, col);
+	mat1 = new (nothrow)int[row*col];
+	mat2 = new (nothrow)int[row*col];
+	matAdd = new (nothrow)int[row*col];
+	if (mat1 == NULL || mat2 == NULL || matAdd == NULL){
+	    cout << "Error in memory allocation";
+	}
+    else{
+        // use the buffers
+    	cout << "Enter elements in 1st matric of size" << row << 'x' << col << ":\n";
+    	MatInput(mat1, row, col);
+    	cout << "Enter elements in 2nd matric of size" << row << 'x' << col << ":\n";
+    	MatInput(mat2, row, col);
+    
+    	MatAdd(mat1, mat2, matAdd, row, col); 
+    
+    	cout << "Sum of two matrices \n";
+    	MatDisplay(matAdd, row, col);
+    }
+	
 
 	delete[] mat1, mat2, matAdd;
 
@@ -49,9 +54,10 @@ void MatAdd(int* mat1, int* mat2, int* mat3, int row, int col) {
 void MatDisplay(int* mat, int row , int col) {
 	int size = row * col;
 	for (int i = 0; i < size; ++i) {
+	    
 		cout << *(mat + i) << " ";
-		if ((i+1) % col == 0) cout << "\n"; // ******
 		
+		if ((i + 1) % col == 0) cout << "\n"; // ******
 		
 	}
 
